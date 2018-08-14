28982e1a2c11ac0fc5eda1d604c6bfacf651e8c4
Nicholas Kim <nicholaskim94@gmail.com>
2018-08-15 04:40:26 +0900

    Initial Commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Nicholas Kim on 8/15/18.
+//  Copyright © 2018 Jamong Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
